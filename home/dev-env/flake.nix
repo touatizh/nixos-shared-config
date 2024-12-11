39d29da18f9c14d19d1fd4093765aa464551c3ad
Helmi Touati <touatizh@gmail.com>
2024-12-11 11:24:04 +0100

    Fix 'flake does not provide attribute' error

--- home/dev-env/flake.nix
+++ home/dev-env/flake.nix
@@ -1,55 +1,53 @@
 {
   description = "Development environments for various use cases";
- 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in {
-    devShells = {
-
       # Python Development Shell
-      python = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          python313
-          python313Packages.pip
-          python313Packages.poetry
-          python313Packages.black
-          python313Packages.ruff
-        ];
-	shellHook = ''
-          echo "Welcome to the Python Development Shell!"
-        '';
-      };
+      devShells.${system} = {
+        python = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              python313
+              poetry
+              black
+              ruff
+            ];
+            shellHook = ''
+            echo "Welcome to the Python Development Shell!"
+            '';
+        };
 
-      # JavaScript/Node.js Development Shell
-      js = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nodejs
-          eslint
-          typescript
-        ];
-	shellHook = ''
-          echo "Welcome to the JS/TS Development Shell!"
-        '';
-      };
+        # JavaScript/Node.js Development Shell
+        js = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs
+              eslint
+              typescript
+            ];
+            shellHook = ''
+            echo "Welcome to the JS/TS Development Shell!"
+            '';
+        };
 
-      # C# Development Shell
-      csharp = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          dotnet-sdk
-        ];
- 	shellHook = ''
-          echo "Welcome to the C# Development Shell!"
-        '';
-      };
+        # C# Development Shell
+        csharp = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              dotnet-sdk
+            ];
+           	shellHook = ''
+            echo "Welcome to the C# Development Shell!"
+            '';
+        };
 
       # Database Development Shell
-      db = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          postgresql
-          sqlite
-          mysql
-        ];
+        db = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              postgresql
+              sqlite
+              mysql
+            ];
         shellHook = ''
           echo "Welcome to the Database Development Shell!"
         '';
@@ -57,4 +55,3 @@
     };
   };
 }
-
