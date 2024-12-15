ac4bdba99d8b2c7f06304c9c1bb7055c0cfd8835
Helmi Touati <touatizh@gmail.com>
2024-12-15 16:15:46 +0100

    Add per-device script to accomodate for monitors and keyboard layout differences between my devices

---
+++ home/dotfiles/hypr/per-device.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+HOSTNAME=$(hostname)
+
+if [[ "$HOSTNAME" == "sp7" ]]; then
+    hyprctl keyword monitor ,prefered,auto,2
+    hyprctl keyword input:kb_layout fr
+elif [[ "$HOSTNAME" == "gigaos" ]]; then
+    hyprctl keyword monitor ,prefered,auto,1.2
+    hyprctl keyword input:kb_layout es
+else
+    hyprctl keyword monitor ,prefered,auto,1
+    hyprctl keyword input:kb_layout fr
+fi
