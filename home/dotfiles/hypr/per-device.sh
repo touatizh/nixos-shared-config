8095764b3e3299836934af12354a3c1a6f6eb2e3
Helmi Touati <touatizh@gmail.com>
2024-12-23 17:59:26 +0100

    Adjust per device implementation for device-specific settings

--- home/dotfiles/hypr/per-device.sh
+++ home/dotfiles/hypr/per-device.sh
@@ -2,7 +2,7 @@
 HOSTNAME=$(hostname)
 
 if [[ "$HOSTNAME" == "sp7" ]]; then
-    hyprctl keyword monitor ,prefered,auto,2
+    hyprctl keyword monitor ,prefered,auto,1.6
     hyprctl keyword input:kb_layout fr
 elif [[ "$HOSTNAME" == "gigaos" ]]; then
     hyprctl keyword monitor ,prefered,auto,1.2
